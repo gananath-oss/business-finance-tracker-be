312360c405c7ccc92996d86f978da6a3620fbf43
gananath-oss <ganaya2k17@gmail.com>
2026-03-21 00:08:41 +0530

    init

---
+++ src/test/java/com/business_finance_tracker/financetracker/FinancetrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.business_finance_tracker.financetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancetrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
